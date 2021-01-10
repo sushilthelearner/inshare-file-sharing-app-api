ea1fcb06659190516fdca21792e30fa8c5ede3c5
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-01-10 23:23:51 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -9,7 +9,7 @@ router.get('/:uuid', async (req, res) => {
         return res.render('download', { error: 'Link has been expired.'});
    } 
    const response = await file.save();
-   const filePath = `${__dirname}/../${file.path}`;
+   const filePath = `${__dirname}/./${file.path}`;
    res.download(filePath);
 });
 
